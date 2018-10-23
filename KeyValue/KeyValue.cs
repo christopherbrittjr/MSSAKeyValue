3c8c495a40f6ca1d8fe4443be85627e8bc6c7713
Chris Britt <christopher.brittjr@outlook.com>
2018-10-23 18:01:51 -0400

    Add project files.

---
+++ KeyValue/KeyValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValue
+{
+    struct KeyValue
+    {
+        public KeyValue(string key, object value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public readonly string key;
+        public readonly object value;
+    }    
+}
---
+++ KeyValue/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValue
+{
+    class MyDictionary
+    {
+        KeyValue[] kvs = new KeyValue[16];
+
+        int inserted = 0;
+
+        public object this[string key]
+        {
+            //Look through the array, find the key
+            //If no key found, throw exception
+            get
+            {
+                for (int i = 0; i < inserted; ++i)
+                {
+                    if (kvs[i].key == key)
+                        return kvs[i].value;
+                }
+
+                throw new KeyNotFoundException("Can't find key: " + key);
+            }
+
+            //Check for existing key and replace if found
+            //Otherwise, add a key to the end
+            set
+            {
+                for (int i = 0; i < inserted; ++i)
+                {
+                    if (kvs[i].key == key)
+                    {
+                        kvs[i] = new KeyValue(key, value);
+                        return;
+                    }
+                }
+
+                //Didn't find the requested key
+                kvs[inserted++] = new KeyValue(key, value);
+            }
+        }
+    }
+}
